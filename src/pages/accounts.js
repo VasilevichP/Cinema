30f68446c62f0e387c0662c6257f98b214f49e56
VasilevichP <143924623+VasilevichP@users.noreply.github.com>
2024-11-12 09:51:06 +0300

    Add files via upload

--- src/pages/accounts.js
+++ src/pages/accounts.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useParams } from 'react-router-dom';
 import '../css/main.css';
 import axios from 'axios';
 import Navbar from '../components/Navbar';
@@ -13,6 +13,55 @@ function Account() {
     const loadAccounts = async () => {
         const accs = await axios.get("http://localhost:8080/account/accounts");
         setAccounts(accs.data);
+    }
+    const [login, setLogin] = useState('')
+    const [password, setPassword] = useState('')
+    const add = async (e) => {
+        e.preventDefault()
+        const account = { login, password }
+        console.log(account)
+        try {
+            const response = await axios.post("http://localhost:8080/account/add",
+                account
+            )
+            let status = response.data;
+            console.log("data", status)
+            switch (status) {
+                case 0:
+                    break;
+                case 1:
+                    loadAccounts();
+                    setLogin('');
+                    setPassword('');
+                    break;
+                default:
+                    console.log("nope");
+                    break;
+            }
+        } catch (err) {
+            console.log("error: ")
+        }
+    }
+    const {loginDel} = useParams();
+    const deleteAccount = async (loginDel) => {
+        console.log("loginDel: ", loginDel)
+        try {
+            const response = await axios.delete(`http://localhost:8080/account/delete/${loginDel}`)
+            let status = response.data;
+            console.log("data", status)
+            switch (status) {
+                case 0:
+                    break;
+                case 1:
+                    loadAccounts();
+                    break;
+                default:
+                    console.log("nope");
+                    break;
+            }
+        } catch (err) {
+            console.log("error: ")
+        }
     }
     return (
         <div>
@@ -35,14 +84,15 @@ function Account() {
                                     accounts.map((account, index) => (
                                         <tr>
                                             <td style={{ width: '40%' }}>{account.login}</td>
-                                            {account.role==1 &&
+                                            {account.role == 1 &&
                                                 <td style={{ width: '40%' }}>Администратор</td>
                                             }
-                                            {account.role==0 &&
+                                            {account.role == 0 &&
                                                 <td style={{ width: '40%' }}>Сотрудник</td>
                                             }
                                             <td style={{ width: '20%' }}>
-                                                <img src={Delete} className='delete-icon' />
+                                                <img src={Delete} className='delete-icon'
+                                                    onClick={() => deleteAccount(account.login) } />
                                             </td>
                                         </tr>))
                                 }
@@ -53,15 +103,19 @@ function Account() {
             </div>
             <div className='container' style={{ marginTop: '2em' }}>
                 <h3>Добавление сотрудника</h3>
-                <label>
-                    <input required type="text" className="auth-reg-inp" placeholder="login_1234" />
-                    <span>Логин</span>
-                </label>
-                <label>
-                    <input required type="text" className="auth-reg-inp" placeholder="Password0_9" />
-                    <span>Пароль</span>
-                </label>
-                <button className='auth-reg-button' type="submit">Добавить</button>
+                <form className='container-form' onSubmit={add}>
+                    <label>
+                        <input required type="text" className="auth-reg-inp" placeholder="login_1234"
+                            value={login} onChange={(e) => setLogin(e.target.value)} />
+                        <span>Логин</span>
+                    </label>
+                    <label>
+                        <input required type="text" className="auth-reg-inp" placeholder="Password0_9"
+                            value={password} onChange={(e) => setPassword(e.target.value)} />
+                        <span>Пароль</span>
+                    </label>
+                    <button className='auth-reg-button' type="submit">Добавить</button>
+                </form>
             </div>
         </div>
     )
